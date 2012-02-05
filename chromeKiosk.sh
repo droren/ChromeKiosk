2f00586f870973157ee86cc7dd7d372d0f93c219
Dennis Hjort <dennis.hjort@ericsson.com>
2012-02-05 21:59:50 +0100

    Adding files needed...

---
+++ chromeKiosk.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Workaround for bug locking screen even if disabled. 
+gsettings set org.gnome.desktop.lockdown disable-lock-screen 'true'
+xset s noblank
+xset s off
+xset -dpms
+gnome-screensaver-command --quit
+
+# execute zonebrowser
+zonebrowser &
+
+# Setting default screen mode, and shutting down laptopdisplay. 
+xrandr --output VGA1 --mode 1280x1024 --primary
+xrandr --output LVDS1 --off
+
+# Debug terminal if needed. 
+xterm
+
+# Main browser loop.
+while true; do chromium-browser %u --proxy-pac-url="http://www-proxy.ericsson.se:3132/accelerated_pac_base.pac" --kiosk --start-maximized; sleep 5s; done
